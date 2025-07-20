9ba22d4f4418d09a027fc88eb6bdfe774aba581c
Muhye AlDeen Akram Al-Raie <86913045+MuhyeAlRaie@users.noreply.github.com>
2025-07-20 08:21:59 +0300

    Update main.js

--- assets/script/main.js
+++ assets/script/main.js
@@ -101,9 +101,6 @@ function filterProducts() {
 
   fetch(PRODUCTS_API_URL, {
     method: "POST",
-     headers: {
-    "Content-Type": "application/json", // ← مهم جدًا
-  },
     body: JSON.stringify(data),
   }).then(() => {
     loadProducts();
